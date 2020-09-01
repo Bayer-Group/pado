096d905d088176f611a49517530a9a652bebb5d5
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-01 16:00:57 +0200

    pado: fix pip install due to repo structure
    This is related to: https://github.com/pypa/setuptools_scm/issues/176
    And shoudl work but doesnt with pep517 mode...

--- pado/setup.py
+++ pado/setup.py
@@ -1,8 +1,11 @@
+import os
 from setuptools import setup
 
 setup(
     use_scm_version={
         'write_to': 'pado/_version.py',
         'version_scheme': 'post-release',
+        'relative_to': os.path.dirname(__file__),
+        'root': '..',
     }
 )
