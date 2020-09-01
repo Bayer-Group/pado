f1f1f26304a1aab680e300afd06f2cd2c6b656b4
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-01 08:20:18 +0200

    pado: cleanup datasources

--- pado/pado/datasource.py
+++ pado/pado/datasource.py
@@ -1,9 +1,4 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-import pandas as pd
-
-# fixme: this file is junk
+from abc import ABC
 
 # TODO:
 #   this needs more thought...
@@ -17,13 +12,4 @@ class DataSource(ABC):
     allow channelling them into the same output format.
 
     """
-
-    @abstractmethod
-    def retrieve_raw(self) -> Any:
-        """retrieve the data source in raw form"""
-        return None
-
-    @abstractmethod
-    def as_dataframe(self) -> pd.DataFrame:
-        """return the data as a dataframe"""
-        raise NotImplementedError("implement in subclass")
+    pass
