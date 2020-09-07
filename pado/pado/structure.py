e0b4bf10d01694537116dcd57bcf7c18048abe97
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-07 14:56:00 +0200

    pado.structure: add PadoReserved

--- pado/pado/structure.py
+++ pado/pado/structure.py
@@ -1,8 +1,20 @@
 import enum
 
 
+class PadoReserved(str, enum.Enum):
+    """reserved pado columns"""
+
+    __str__ = str.__str__
+
+    SRC_ID = "_pado_source_id"
+    REL_PATH = "_pado_rel_path"
+    _RESERVED_COL_INDEX = "level_0"
+
+
 class PadoColumn(str, enum.Enum):
     """standardized pado columns"""
+
+    __str__ = str.__str__
 
     STUDY = "study"
     COMPOUND = "compound"
@@ -11,6 +23,3 @@ class PadoColumn(str, enum.Enum):
     IMAGE = "image"
     ORGAN = "organ"
     FINDING = "finding"
-
-    def __str__(self):
-        return str.__str__(self)
