6ef6cdf48dd928f1104ca068138db501f481ee49
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-07 12:52:28 +0200

    pado.structure: allow standardized columns as pandas column names

--- pado/pado/structure.py
+++ pado/pado/structure.py
@@ -5,8 +5,12 @@ class PadoColumn(str, enum.Enum):
     """standardized pado columns"""
 
     STUDY = "study"
+    COMPOUND = "compound"
     ANIMAL = "animal"
     SLIDE = "slide"
     IMAGE = "image"
     ORGAN = "organ"
     FINDING = "finding"
+
+    def __str__(self):
+        return str.__str__(self)
