7b46018c2aa441fdb238ced136bd255af852bc23
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-09 09:53:57 +0200

    tests: add more pado dataset tests

--- pado/tests/test_dataset.py
+++ pado/tests/test_dataset.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import pandas as pd
+import pytest
 
 from pado.dataset import PadoDataset
+from pado.structure import PadoColumn, PadoReserved
 
 
 def test_pado_test_datasource(datasource):
@@ -23,3 +25,27 @@ def test_write_pado_dataset(datasource, tmp_path):
     assert len(list(filter(Path.is_file, (ds.path / "images").glob("**/*")))) == 1
     assert isinstance(ds.metadata, pd.DataFrame)
     assert len(ds.metadata) == 10
+
+
+@pytest.fixture()
+def dataset(datasource, tmp_path):
+    dataset_path = tmp_path / "my_dataset"
+    ds = PadoDataset(dataset_path, mode="x")
+    ds.add_source(datasource)
+    yield ds
+
+
+@pytest.fixture(scope="module")
+def dataset_ro(datasource, tmp_path):
+    dataset_path = tmp_path / "my_dataset"
+    ds = PadoDataset(dataset_path, mode="x")
+    ds.add_source(datasource)
+    del ds
+    yield PadoDataset(dataset_path, mode="r")
+
+
+def test_pado_dataframe_accessor(dataset):
+    df_subset = dataset.metadata.pado.organs
+    assert isinstance(df_subset, pd.DataFrame)
+    assert len(df_subset) > 0
+    assert all(map(lambda x: x.startswith(PadoColumn.ORGAN), df_subset.columns))
