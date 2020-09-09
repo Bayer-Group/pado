e960360d949561164be85e94e7683bb4ddf1ae2e
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-09 09:23:39 +0200

    pado: fix editable installs of plugins
    To make editable installs with plugins work `pado` seems to
    have to be a namespace too.
    But since we want `pado.__version__` to work, the __init__
    will need to be vendored by all plugins.

--- pado/__init__.py
+++ pado/__init__.py
@@ -1,5 +1,9 @@
-"""pado --- rearranging into digestible chunks"""
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+# to allow `pip install -e` to work on the `pado.ext` namespace,
+# it seems we have to turn pado into a namespace too...
+# that means: this file must be vendored in all `pado_*`
+# datasource modules and cannot be modified.
 try:
-    from pado.pado import version as __version__
+    from ._version import version as __version__
 except ImportError:
     __version__ = "not-installed"
