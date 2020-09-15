91f90d742af43ca59372e52cebdcff4f108b2059
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-15 14:07:56 +0200

    pado.structure: implement structurize_metadata

--- pado/structure.py
+++ pado/structure.py
@@ -3,6 +3,8 @@ import string
 from collections import defaultdict
 from typing import Dict, Iterable, List
 
+import pandas as pd
+
 __all__ = [
     "PadoColumn",
     "PadoInvalid",
@@ -108,3 +110,103 @@ def build_column_map(columns: Iterable[str]) -> Dict[str, List[str]]:
         raise ValueError(f"unsupported keys {set(output) - set(PadoColumn)}")
     output.default_factory = None
     return output
+
+
+def _collapse_dataframe_to_dict(df: pd.DataFrame, structure: Dict, col_map: Dict):
+    """helper function to collapse the dataframe into a dictionary"""
+    # flatten
+    columns = []
+    for toplevel in structure.values():
+        columns.extend(col_map[toplevel])
+
+    sdf = df[columns].drop_duplicates()
+    if len(sdf) > 1:
+        raise ValueError("can't collapse...")
+
+    md = {}
+    for md_key_tuple, toplevel in structure.items():
+        d = md
+        for md_key in md_key_tuple:
+            d = md.setdefault(md_key, {})
+
+        for col in col_map[toplevel]:
+            key, _, subkey = col.partition(SEPARATOR)
+            if subkey == "":
+                subkey = col
+            d[subkey] = sdf[col].item()
+
+    return md
+
+
+def structurize_metadata(
+    df: pd.DataFrame, root_column: PadoColumn, col_map: Dict
+) -> Dict:
+    """try to enforce sensible relationship structure
+
+    FIXME:
+      The relationship between the toplevel "physical" objects in the pathology
+      data should be defined somewhere. For the sake of moving fast we implement
+      it one by one for what we need at the time. This should be replaced...
+
+    """
+    if root_column != PadoColumn.IMAGE:
+        raise NotImplementedError("relationship structure not implemented yet")
+
+    # Enforce the structure for the metadata:
+    # TODO: verify if this is sensible
+    # IMAGE M->1 SLIDE
+    #   SLIDE M->N ORGAN
+    #     ORGAN 1->1 ANIMAL
+    #       ANIMAL M->1 GROUP
+    #         GROUP M->1 EXPERIMENT
+    #           EXPERIMENT M->1 COMPOUND
+    #           EXPERIMENT M->1 STUDY
+    #     ORGAN 1->M FINDING
+    #
+    # This means, that using IMAGE as root we should be able to do:
+    # image_md = {
+    #   "slide": {
+    #     "organ": [{
+    #       "animal": {
+    #         "group": {
+    #           "experiment": {
+    #             "compound": {},
+    #             "study": {},
+    #           },
+    #         },
+    #       },
+    #       "finding": [
+    #         ...
+    #       ]
+    #     }, ...
+    #     ]
+    #   }
+
+    structure = {
+        (): PadoColumn.IMAGE,
+        ("slide",): PadoColumn.SLIDE,
+    }
+    md = _collapse_dataframe_to_dict(df, structure, col_map)
+    md["slide"]["organ"] = []
+
+    for _, odf in df.groupby([PadoColumn.ORGAN, PadoColumn.ANIMAL]):
+        organ_structure = {
+            (): PadoColumn.ORGAN,
+            ("animal",): PadoColumn.ANIMAL,
+            ("animal", "group"): PadoColumn.GROUP,
+            ("animal", "group", "experiment"): PadoColumn.EXPERIMENT,
+            ("animal", "group", "experiment", "compound"): PadoColumn.COMPOUND,
+            ("animal", "group", "experiment", "study"): PadoColumn.STUDY,
+        }
+        organ = _collapse_dataframe_to_dict(odf, organ_structure, col_map)
+        organ["finding"] = []
+        for _, fdf in odf.groupby(col_map[PadoColumn.FINDING]):
+            finding_structure = {
+                (): PadoColumn.FINDING,
+            }
+            finding = _collapse_dataframe_to_dict(fdf, finding_structure, col_map)
+            organ["finding"].append(finding)
+
+        md["slide"]["organ"].append(organ)
+
+    return md
