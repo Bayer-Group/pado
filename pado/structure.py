10f67f097ff3165337709933623b5895b895aa31
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-15 14:07:04 +0200

    pado.structure: fix str cast on PadoColumn

--- pado/structure.py
+++ pado/structure.py
@@ -19,19 +19,23 @@ class PadoReserved(str, enum.Enum):
     """reserved pado columns"""
 
     __version__ = 1
-    __str__ = str.__str__
 
     DATA_SOURCE_ID = "_pado_data_source_id"
     IMAGE_REL_PATH = "_pado_image_rel_path"
+
+    def __str__(self):
+        return str(self.value)
 
 
 class PadoInvalid(str, enum.Enum):
     """invalid pado columns"""
 
     __version__ = 1
-    __str__ = str.__str__
 
     RESERVED_COL_INDEX = "level_0"
+
+    def __str__(self):
+        return str(self.value)
 
 
 class _SubColumnMixin:
@@ -55,7 +59,6 @@ class PadoColumn(_SubColumnMixin, str, enum.Enum):
     """standardized pado columns"""
 
     __version__ = 1
-    __str__ = str.__str__
 
     STUDY = "STUDY"
     EXPERIMENT = "EXPERIMENT"
@@ -66,6 +69,9 @@ class PadoColumn(_SubColumnMixin, str, enum.Enum):
     SLIDE = "SLIDE"
     IMAGE = "IMAGE"
     FINDING = "FINDING"
+
+    def __str__(self):
+        return str(self.value)
 
 
 def verify_columns(columns: Iterable[str], raise_if_invalid: bool = True) -> bool:
@@ -97,7 +103,7 @@ def build_column_map(columns: Iterable[str]) -> Dict[str, List[str]]:
     for col in columns:
         key, _, _ = col.partition(SEPARATOR)
         if key not in ignore:
-            output[key].append(col)
+            output[key].append(str(col))
     if not (output.keys() <= valid):  # Set comparison
         raise ValueError(f"unsupported keys {set(output) - set(PadoColumn)}")
     output.default_factory = None
