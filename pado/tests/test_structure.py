2dd9aeb34339bb4600506b35369cbb7b670f3da4
Andreas Poehlmann <andreas@poehlmann.io>
2020-09-29 09:28:31 +0200

    tests: more structure tests

--- pado/tests/test_structure.py
+++ pado/tests/test_structure.py
@@ -2,7 +2,14 @@ import itertools
 
 import pytest
 
-from pado.structure import PadoColumn, PadoInvalid, PadoReserved, verify_columns
+from pado.structure import (
+    PadoColumn,
+    PadoInvalid,
+    PadoReserved,
+    build_column_map,
+    structurize_metadata,
+    verify_columns,
+)
 
 
 def test_column_enums():
@@ -46,3 +53,25 @@ def test_verify_columns():
         verify_columns(["IMAGE___WRONG"])
 
     assert verify_columns(["NOT_A_TOPLEVEL"], raise_if_invalid=False) is False
+
+
+def test_build_column_map(datasource):
+    with datasource:
+        m = build_column_map(datasource.metadata.columns)
+
+
+def test_build_column_map_raise_missing():
+    with pytest.raises(ValueError):
+        build_column_map(["UNKNOWN_TOPLEVEL"])
+
+
+def test_structurize_metadata(datasource):
+    with datasource:
+        col_map = build_column_map(datasource.metadata.columns)
+        # prepare dataframe
+        df = datasource.metadata
+    img_id = df[PadoColumn.IMAGE].iloc[0]
+    sdf = df.loc[df[PadoColumn.IMAGE] == img_id, :]
+    # structure
+    s = structurize_metadata(sdf, PadoColumn.IMAGE, col_map)
+    assert s
