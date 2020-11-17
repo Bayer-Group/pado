2e35409d6bbc7b989aea7bff03c196fb00d35233
Andreas Poehlmann <andreas@poehlmann.io>
2020-11-17 17:17:35 +0100

    pado: reenable version

--- pado/__init__.py
+++ pado/__init__.py
@@ -2,12 +2,7 @@ __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 # to allow `pip install -e` to work on the `pado.ext` namespace,
 # it seems we have to turn pado into a namespace too...
 #
-# What we actually want is have the below code in pado/__init__.py
-# so that plugin modules can't break the `pado.` module.
-# But PyCharm doesn't play nice then. So for the sake of smooth
-# development we give up on pado.__version__ for now.
-#
-# try:
-#     from ._version import version as __version__
-# except ImportError:
-#     __version__ = "not-installed"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "not-installed"
