f1e5fbf4864a4564cfef5a8fa1d0bb8b3e335bd9
Andreas Poehlmann <andreas@poehlmann.io>
2021-01-07 17:38:27 +0100

    pado: allow accessing PadoDataset directly via pado.PadoDataset

--- pado/__init__.py
+++ pado/__init__.py
@@ -6,3 +6,17 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "not-installed"
+
+# noinspection PyUnresolvedReferences
+__all__ = [
+    "PadoDataset",
+    "PadoDatasetChain",
+]
+
+
+# allow importing items in __all__
+def __getattr__(name):
+    from importlib import import_module
+    if name in __all__:
+        return getattr(import_module("pado.dataset"), name)
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
