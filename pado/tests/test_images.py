6a8b7a14cedb5d614953de5578d99bbb7c062886
Andreas Poehlmann <andreas@poehlmann.io>
2021-01-11 12:54:37 +0100

    tests: ImageId roundtrip

--- pado/tests/test_images.py
+++ pado/tests/test_images.py
@@ -1,5 +1,21 @@
 import pytest
 
+from pado.images import ImageId
+
+
+@pytest.mark.parametrize(
+    "image_id", [
+        ImageId("a"),
+        ImageId("a", "b", "c"),
+        ImageId("a b"),
+        ImageId("a\u1234b")
+    ]
+)
+def test_image_id_roundtrip(image_id):
+    id_str = image_id.to_str()
+    new_id = ImageId.from_str(id_str)
+    assert image_id == new_id
+
 
 def test_image_id(dataset_ro):
     key = next(iter(dataset_ro))
