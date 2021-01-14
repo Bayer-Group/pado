128fc4646898149938481a5e97a066aa2ad7c78f
Andreas Poehlmann <andreas@poehlmann.io>
2021-01-14 12:56:02 +0100

    tests: add fixture blocking pathlib.Path.glob
    make tests fail in case pathlib.Path.glob is used in pado.

--- pado/tests/conftest.py
+++ pado/tests/conftest.py
@@ -24,3 +24,17 @@ def dataset_ro(datasource, tmp_path):
     ds.add_source(datasource)
     del ds
     yield PadoDataset(dataset_path, mode="r")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def mock_response(monkeypatch):
+    """pathlib.Path.glob suffers from a bug regarding symlinks:
+
+    https://bugs.python.org/issue33428
+
+    let's enforce that we do not use it in pado and use glob.glob instead.
+    """
+    with monkeypatch.context() as m:
+        # remove pathlib.Path.glob so that tests fail if used unintentionally
+        m.delattr("pathlib.Path.glob")
+        yield
