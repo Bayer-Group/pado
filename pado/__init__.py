2248db01ec895f01adb8aa0e3ef5da1dbbef3c65
Andreas Poehlmann <andreas@poehlmann.io>
2021-01-18 14:11:46 +0100

    pado.__init__: undo pep420 namespace package for 'pado'
    PyCharm has better handling for namespace packages now...

--- pado/__init__.py
+++ pado/__init__.py
@@ -1,7 +1,4 @@
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
-# to allow `pip install -e` to work on the `pado.ext` namespace,
-# it seems we have to turn pado into a namespace too...
-#
+
 try:
     from ._version import version as __version__
 except ImportError:
