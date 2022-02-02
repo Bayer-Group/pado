fd3e9222e36c040554871198988c9361c75a0b87
Andreas Poehlmann <andreas@poehlmann.io>
2022-02-02 06:53:06 +0100

    pado.dataset: expose predictions via proxy

--- pado/dataset.py
+++ pado/dataset.py
@@ -43,6 +43,7 @@ from pado.io.store import StoreType
 from pado.io.store import get_store_type
 from pado.metadata import GroupedMetadataProvider
 from pado.metadata import MetadataProvider
+from pado.predictions.proxy import PredictionProxy
 from pado.types import DatasetSplitter
 from pado.types import IOMode
 from pado.types import UrlpathLike
@@ -130,6 +131,7 @@ class PadoDataset:
         self._cached_image_provider = None
         self._cached_annotation_provider = None
         self._cached_metadata_provider = None
+        self._cached_predictions_proxy = None
 
     @property
     def urlpath(self) -> str:
@@ -234,6 +236,12 @@ class PadoDataset:
 
             self._cached_metadata_provider = metadata_provider
         return self._cached_metadata_provider
+
+    @property
+    def predictions(self) -> PredictionProxy:
+        if self._cached_predictions_proxy is None:
+            self._cached_predictions_proxy = PredictionProxy(self)
+        return self._cached_predictions_proxy
 
     # === access ===
 
