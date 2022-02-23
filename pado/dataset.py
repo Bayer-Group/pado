4d12a720f89d5aaa72e6297ff6751acde512be24
Andreas Poehlmann <andreas@poehlmann.io>
2022-02-23 10:22:31 +0100

    pado.dataset: improve dataset validation logic
    fs.glob() returns an empty list if s3fs would raise a PermissionError.
    This commit let's the PermissionError propagate to the user.

--- pado/dataset.py
+++ pado/dataset.py
@@ -104,16 +104,10 @@ class PadoDataset:
             # if the dataset files should be there, check them
             fs = self._fs
             if mode in {"r", "r+"}:
-                try:
-                    _image_parquet_files = fs.glob(self._get_fspath("*.image.parquet"))
-                except OSError as err:
-                    if err.args and self._storage_options:
-                        msg, *remaining_args = err.args
-                        err.args = tuple(
-                            (f"{msg!r}, possibly outdated credentials", *remaining_args)
-                        )
-                    raise
-                if not any(_image_parquet_files):
+                if not (
+                    fs.isdir(self._root)  # raises if not there or reachable
+                    and any(fs.glob(self._get_fspath("*.image.parquet")))
+                ):
                     raise ValueError(
                         f"error: {self._urlpath} not a valid dataset since it has no image parquet file."
                     )
