ee8953f176e9fdabc334f5b7df22cb92f6eca51f
Andreas Poehlmann <andreas@poehlmann.io>
2022-07-27 11:05:56 +0200

    pado.transporter: move _cli code into transporter submodule
    utlimately pado.transporter functionality should be made available
    through the typer cli. This is a good immediate step towards
    cleaning up

--- pado/transporter.py
+++ pado/transporter.py
@@ -9,15 +9,49 @@ import re
 import subprocess
 import sys
 import traceback
+from argparse import ArgumentTypeError
 from collections import defaultdict
+from functools import partial
 from pathlib import Path
 from textwrap import dedent
 
 import toml
 from appdirs import user_config_dir
 
-from pado._cli import argument
-from pado._cli import subcommand
+
+def subcommand(*arguments, parent):
+    """decorator helper for commandline"""
+
+    def decorator(func):
+        fn = func.__name__.rstrip("_")
+        started_via_m = Path(sys.argv[0]).name == "__main__.py"
+        subparser = parent.add_parser(
+            name=fn,
+            prog=f"python -m pado {fn}" if started_via_m else f"pado {fn}",
+            help=func.__doc__,
+        )
+        for args, kwargs in arguments:
+            subparser.add_argument(*args, **kwargs)
+        subparser.set_defaults(cmd_func=partial(func, subparser=subparser))
+        return func
+
+    return decorator
+
+
+def argument(*args, **kwargs):
+    """argument helper for subcommand"""
+    return args, kwargs
+
+
+class DirectoryType:
+    """Directory parsing for argparse"""
+
+    def __call__(self, cmd_input: str):
+        p = Path(cmd_input)
+        if p.is_dir():
+            return p
+        raise ArgumentTypeError(f"'{cmd_input}' is not a directory")
+
 
 # argument parsing objects
 parser = argparse.ArgumentParser(
